aa66d374ffa954a755f3bfbf92d00b01f6000153
kalrit718 <kalindu.rithmal.other@gmail.com>
2022-02-12 01:02:35 +0530

    Eleventh page, Twelfth page added.

---
+++ views/EleventhPage.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import type { Node } from 'react';
+
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    TextInput,
+} from 'react-native';
+
+
+const EleventhPage: () => Node = () => {
+
+    const pageData = {
+        "name": "Jassica Anderson",
+        "department": "IT Section",
+        "time": "10:10",
+        "date": "01/01/2022",
+        "front_image": require("../assets/images/Women2_FrontFace.jpg")
+    }
+
+    return (
+        <View style={{ display: 'flex', flexDirection: 'column', width: "100%", height: "100%" }}>
+            <View style={{ width: "70%", height: "110%", position: 'absolute', top: "20%", right: "70%", borderRadius: 500, borderColor: "brown", borderWidth: 3 }}></View>
+            <View style={{ width: "70%", height: "110%", position: 'absolute', bottom: "20%", left: "60%", borderRadius: 500, borderColor: "#2f5c83", borderWidth: 3 }}></View>
+            <View style={{ flexDirection: "row", height: "100%", padding: 10 }}>
+                <View style={{ height: "100%", width: "50%" }}>
+                    <View style={{ width: "100%", height: "20%", display: 'flex', flexDirection: 'row' }}>
+                        <View style={{ width: "50%", height: "70%" }}>
+                            <Image source={require('../assets/images/ProjectMOFI_WhiteBackground.jpeg')} style={{ resizeMode: "contain", height: "100%", width: "100%" }} />
+                        </View>
+                    </View>
+
+                    {/*<View style={{ backgroundColor: "brown", width: "100%", height: "60%", alignItems: 'center', justifyContent: 'center' }}></View>*/}
+                    <View style={{ width: "100%", height: "60%", alignItems: 'center', justifyContent: 'center' }}>
+                        <View style={{ zIndex: 2, backgroundColor: "#011e36", display: 'flex', flexDirection: 'row', width: "70%", height: "80%", borderRadius: 20 }}>
+                            <View style={{ height: "100%", width: "100%", padding: 20 }}>
+                                <View style={{ height: "100%", width: "100%", borderColor: "#02e6c8", borderWidth: 3, borderRadius: 5 }}>
+                                    <Image source={ pageData.front_image } style={{ resizeMode: "contain", height: "100%", width: "100%" }} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={{ height: "20%", width: "100%", display: "flex", flexDirection: 'row', justifyContent: 'center' }}>
+                        <Text style={{ color: "#34d27b", fontSize: 50, fontWeight: '700' }}>Access Granted</Text>
+                        <Image source={require('../assets/icons/check-circle-solid.svg')} style={{ resizeMode: "contain", marginLeft: 5, marginTop: 10, width: 50, height: 50 }} />
+                    </View>
+                </View>
+
+                <View style={{ height: "100%", width: "50%", padding: 50, paddingTop: 100, paddingBottom: 100 }}>
+                    <View style={{ backgroundColor: "#011e36", width: "100%", height: "100%", borderRadius: 50, padding: 30 }}>
+                        <View style={{ width: "100%", height: "15%", paddingTop: 20, display: 'flex', flexDirection: 'row' }}>
+                            <Text style={{ color: "#02e6c8", fontSize: 40, fontWeight: '500' }}>Name : </Text>
+                            <Text style={{ color: "white", width: "100%", fontSize: 40, fontWeight: '400', marginLeft: 10 }}>{pageData.name}</Text>
+                        </View>
+                        <View style={{ width: "100%", height: "15%", paddingTop: 20, display: 'flex', flexDirection: 'row' }}>
+                            <Text style={{ color: "#02e6c8", fontSize: 40, fontWeight: '500' }}>Department : </Text>
+                            <Text style={{ color: "white", width: "100%", fontSize: 40, fontWeight: '400', marginLeft: 10 }}>{pageData.department}</Text>
+                        </View>
+                        <View style={{ width: "100%", height: "15%", paddingTop: 20, display: 'flex', flexDirection: 'row' }}>
+                            <Text style={{ color: "#02e6c8", fontSize: 40, fontWeight: '500' }}>Attendance marked at : </Text>
+                        </View>
+                        <View style={{ borderColor: "white", borderWidth: 1, width: "100%", height: "15%", display: 'flex', flexDirection: 'row' }}>
+                            <View style={{ borderColor: "white", borderWidth: 1, width: "50%", height: "100%", display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ color: "white", fontSize: 40, fontWeight: '500' }}>Time</Text>
+                            </View>
+                            <View style={{ borderColor: "white", borderWidth: 1, width: "50%", height: "100%", display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ color: "white", fontSize: 40, fontWeight: '500' }}>Date</Text>
+                            </View>
+                        </View>
+                        <View style={{ borderColor: "white", borderWidth: 1, borderTopWidth: 0, width: "100%", height: "15%", display: 'flex', flexDirection: 'row' }}>
+                            <View style={{ borderColor: "white", borderWidth: 1, width: "50%", height: "100%", display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ color: "white", fontSize: 35, fontWeight: '400' }}>{pageData.time}</Text>
+                            </View>
+                            <View style={{ borderColor: "white", borderWidth: 1, width: "50%", height: "100%", display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ color: "white", fontSize: 35, fontWeight: '400' }}>{pageData.date}</Text>
+                            </View>
+                        </View>
+                        <View style={{ width: "100%", height: "25%", paddingTop: 20, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                            <Text style={{ color: "#b2f888", fontSize: 40, fontWeight: '700' }}>THANK YOU!</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+
+}
+
+export default EleventhPage;
---
+++ views/TwelfthPage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import type { Node } from 'react';
+
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    TextInput,
+} from 'react-native';
+
+
+const TwelfthPage: () => Node = () => {
+
+    var welcomePageData = {
+        "company_name":"Informatics Institute of Technology" 
+    }
+
+    return (
+        <View style={{ backgroundColor: "#f7f7f7", display: 'flex', flexDirection: 'row', width: "100%", height: "100%" }}>
+            <View style={{ height: "100%", width: "50%" }}>
+                <Image source={require('../assets/images/WelcomeScreenBackgroundImage.jpg')} style={{ borderBottomRightRadius: 1200, borderTopRightRadius: 1000, marginTop: -100, marginLeft: "-20%", resizeMode: 'cover', width: "120%", height: "99%" }} />
+            </View>
+            <View style={{ height: "100%", width: "45%", marginLeft: "0%", paddingLeft: "5%" }}>
+                <Image source={require('../assets/images/ProjectMOFI_Background_Image_White.jpeg')} style={{ resizeMode: "contain", height: "100%", width: "100%" }} />
+                <View style={{ position: 'relative', bottom: "55%", left: "10%" }}>
+                    <Text style={{ color: "green", fontSize: 70, fontWeight: '500' }}>WELCOME</Text>
+                    <Text style={{ color: "black", fontSize: 35, fontWeight: '500' }}>To the</Text>
+                    <Text style={{ color: "red", fontSize: 30, fontWeight: '500' }}>{welcomePageData.company_name}</Text>
+
+                </View>
+            </View>
+            <View style={{ position: 'absolute', borderColor: "#eae780", borderWidth: 3, borderBottomRightRadius: 1200, borderTopRightRadius: 1000, marginTop: -100, marginLeft: "-5%", height: "100%", width: "60%" }}></View>
+            <View style={{ position: 'absolute', borderColor: "#2a5373", borderWidth: 3, borderBottomRightRadius: 1200, borderTopRightRadius: 1000, marginTop: -200, marginLeft: "-3%", height: "140%", width: "60%" }}></View>
+        </View>
+    );
+
+}
+
+export default TwelfthPage;

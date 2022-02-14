35d3026d38de3248d338fcf34d7db84d7c89a192
unknown <anujiabey1234@gmail.com>
2022-02-14 19:18:45 +0530

    Modified Pages 1-7 and 11-12

--- views/SecondPage.js
+++ views/SecondPage.js
@@ -7,6 +7,7 @@ import {
     View,
     Text,
     Image,
+    StyleSheet,
     TouchableOpacity,
 } from 'react-native';
 
@@ -20,73 +21,73 @@ const SecondPage: () => Node = () => {
     }
 
     return (
-        <View style={{ flexDirection: "row", backgroundColor: "#011e36", height: "100%", padding: 10 }}>
-            <View style={{ backgroundColor: "#011e36", height: "100%", width: "20%", borderColor: "#02e6c8", borderRightWidth: 5, paddingTop: 10, paddingRight: 10 }}>
+        <View style={styles.style1}>
+            <View style={styles.style2}>
                 <SideBar name={pageData.name_of_user} />
             </View>
-            <View style={{ backgroundColor: "#011e36", height: "100%", width: "80%", display: "flex", flexDirection: 'column', padding: 50 }}>
-                <View style={{ paddingBottom: 50, paddingRight:40, width: "100%" }}>
-                    <Text style={{ fontSize: 30, color: "white", paddingBottom: 10 }}>
+            <View style={styles.style3}>
+                <View style={styles.style4}>
+                    <Text style={styles.style5}>
                         Display Camera Status
                     </Text>
-                    <View style={{ backgroundColor: "#02e6c8", height: 2, width: "100%" }}></View>
+                    <View style={styles.style6}></View>
                 </View>
-                <View style={{ width: "100%", display: 'flex', flexDirection: 'row' }}>
-                    <View style={{ width: "50%" }}>
-                        <Text style={{ fontSize: 20, color: "white", paddingBottom: 10, marginLeft: 5 }}>Side View</Text>
-                        <View style={{ backgroundColor: "white", width: "90%", height: 250, borderRadius: 5 }}>
-                            <Image source={pageData.side_image} style={{ resizeMode: "contain", height: "100%", width: "100%" }} />
+                <View style={styles.style7}>
+                    <View style={styles.style8}>
+                        <Text style={styles.style9}>Side View</Text>
+                        <View style={styles.style10}>
+                            <Image source={pageData.side_image} style={styles.style11} />
                         </View>
-                        <View style={{ width: "90%", marginTop: 20, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                            <TouchableOpacity style={{ width: "40%", height: 30, backgroundColor: "#5776f2", borderRadius: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ color: "white", fontSize: 18 }} >Capture</Text>
+                        <View style={styles.style12}>
+                            <TouchableOpacity style={styles.style13}>
+                                <Text style={styles.style14} >Capture</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style={{ width: "40%", height: 30, backgroundColor: "red", borderRadius: 5, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                                <Text style={{ color: "white", fontSize: 18 }} >Clear</Text>
+                            <TouchableOpacity style={styles.style15}>
+                                <Text style={styles.style14} >Clear</Text>
                             </TouchableOpacity>
                         </View>
                     </View>
-                    <View style={{ width: "50%" }}>
-                        <Text style={{ fontSize: 20, color: "white", paddingBottom: 10, marginLeft: 5 }}>Front View</Text>
-                        <View style={{ backgroundColor: "white", width: "90%", height: 250, borderRadius: 5 }}>
-                            {/*<Image source={pageData.front_image} style={{ resizeMode: "contain", height: "100%", width: "100%" }} />*/}
+                    <View style={styles.style8}>
+                        <Text style={styles.style9}>Front View</Text>
+                        <View style={styles.style10}>
+                            <Image source={pageData.front_image} style={styles.style11} />
                         </View>
-                        <View style={{ width: "90%", marginTop: 20, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                            <TouchableOpacity style={{ width: "40%", height: 30, backgroundColor: "#5776f2", borderRadius: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ color: "white", fontSize: 18 }} >Capture</Text>
+                        <View style={styles.style12}>
+                            <TouchableOpacity style={styles.style13}>
+                                <Text style={styles.style14} >Capture</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style={{ width: "40%", height: 30, backgroundColor: "red", borderRadius: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ color: "white", fontSize: 18 }} >Clear</Text>
+                            <TouchableOpacity style={styles.style15}>
+                                <Text style={styles.style14} >Clear</Text>
                             </TouchableOpacity>
                         </View>
 
                     </View>
                 </View>
-                <View style={{ marginTop: 50, width: "100%", display: 'flex', flexDirection: 'row' }}>
-                    <View style={{ width: "50%" }}>
-                        <View style={{ paddingBottom: 50, paddingRight: 40, width: "100%" }}>
-                            <Text style={{ fontSize: 25, color: "white", paddingBottom: 10 }}>
+                <View style={styles.style7}>
+                    <View style={styles.style8}>
+                        <View style={styles.style16}>
+                            <Text style={styles.style17}>
                                 Capture Comparing
                             </Text>
-                            <View style={{ backgroundColor: "#02e6c8", height: 2, width: "100%" }}></View>
+                            <View style={styles.style18}></View>
                         </View>
-                        <View style={{ width: "100%", display: 'flex', flexDirection: 'row' }}>
-                            <TouchableOpacity style={{ width: "90%", height: 40, backgroundColor: "#02e6c8", borderRadius: 5, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                                <Text style={{ color: "white", fontSize: 18 }} >Compare With Database</Text>
-                                <Image source={require('../assets/icons/sync_white_24dp.svg')} style={{ resizeMode: "contain", marginLeft: 5, width: 27, height: 27 }} />
+                        <View style={styles.style7}>
+                            <TouchableOpacity style={styles.style19}>
+                                <Text style={styles.style14} >Compare With Database</Text>
+                                <Image source={require('../assets/icons/sync_white_24dp.svg')} style={styles.style20} />
                             </TouchableOpacity>
                         </View>
                     </View>
-                    <View style={{ width: "50%" }}>
-                        <View style={{ paddingBottom: 50, paddingRight: 40, width: "100%" }}>
-                            <Text style={{ fontSize: 25, color: "white", paddingBottom: 10 }}>
+                    <View style={styles.style8}>
+                        <View style={styles.style16}>
+                            <Text style={styles.style17}>
                                 Output
                             </Text>
-                            <View style={{ backgroundColor: "#02e6c8", height: 2, width: "100%" }}></View>
+                            <View style={styles.style18}></View>
                         </View>
-                        <View style={{ width: "100%", display: 'flex', flexDirection: 'row' }}>
-                            <Text style={{ color: "#34d27b", fontSize: 30, fontWeight: '500' }} >Access Granted</Text>
-                            <Image source={require('../assets/icons/check-circle-solid.svg')} style={{ resizeMode: "contain", marginLeft: 5, marginTop: 5, width: 30, height: 30 }} />
+                        <View style={styles.style7}>
+                            <Text style={styles.style21} >Access Granted</Text>
+                            <Image source={require('../assets/icons/check-circle-solid.svg')} style={styles.style22} />
                         </View>
                     </View>
                 </View>
@@ -95,5 +96,143 @@ const SecondPage: () => Node = () => {
     );
 
 }
+
+const styles = StyleSheet.create({
+    style1: {
+        flexDirection: "row",
+        backgroundColor: "#011e36",
+        height: "100%",
+        padding: 10
+    },
+    style2: {
+        backgroundColor: "#011e36",
+        height: "100%",
+        width: "20%",
+        borderColor: "#02e6c8",
+        borderRightWidth: 5,
+        paddingTop: 10,
+        paddingRight: 10
+    },
+    style3: {
+        backgroundColor: "#011e36",
+        height: "100%",
+        width: "80%",
+        display: "flex",
+        flexDirection: 'column',
+        padding: 50
+    },
+    style4: {
+        addingBottom: 50,
+        paddingRight: 40,
+        width: "100%"
+    },
+    style5: {
+        fontSize: 30,
+        color: "white",
+        paddingBottom: 10
+    },
+    style6: {
+        backgroundColor: "#02e6c8",
+        height: 2,
+        width: "100%"
+    },
+    style7: {
+        width: "100%",
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    style8: {
+        width: "50%"
+    },
+    style9: {
+        fontSize: 20,
+        color: "white",
+        paddingBottom: 10,
+        marginLeft: 5
+    },
+    style10: {
+        backgroundColor: "white",
+        width: "90%",
+        height: 250,
+        borderRadius: 5
+    },
+    style11: {
+        resizeMode: "contain",
+        height: "100%",
+        width: "100%"
+    },
+    style12: {
+        width: "90%",
+        marginTop: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    style13: {
+        width: "40%",
+        height: 30,
+        backgroundColor: "#5776f2",
+        borderRadius: 5,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    style14: {
+        color: "white",
+        fontSize: 18
+    },
+    style15: {
+        width: "40%",
+        height: 30,
+        backgroundColor: "red",
+        borderRadius: 5,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    style16: {
+        paddingBottom: 50,
+        paddingRight: 40,
+        width: "100%"
+    },
+    style17: {
+        fontSize: 25,
+        color: "white",
+        paddingBottom: 10
+    },
+    style18: {
+        backgroundColor: "#02e6c8",
+        height: 2,
+        width: "100%"
+    },
+    style19: {
+        width: "90%",
+        height: 40,
+        backgroundColor: "#02e6c8",
+        borderRadius: 5,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    style20: {
+        resizeMode: "contain",
+        marginLeft: 5,
+        width: 27,
+        height: 27
+    },
+    style21: {
+        color: "#34d27b",
+        fontSize: 30,
+        fontWeight: '500'
+    },
+    style22: {
+        resizeMode: "contain",
+        marginLeft: 5,
+        marginTop: 5,
+        width: 30,
+        height: 30
+    }
+});
 
 export default SecondPage;
--- views/SeventhPage.js
+++ views/SeventhPage.js
@@ -9,36 +9,119 @@ import {
     Image,
     TouchableOpacity,
     TextInput,
+    StyleSheet,
 } from 'react-native';
 
 
 const SeventhPage: () => Node = () => {
     return (
-        <View style={{ flexDirection: "row", backgroundColor: "#011e36", height: "100%", padding: 10 }}>
-            <View style={{ backgroundColor: "#011e36", height: "100%", width: "60%", paddingTop: 10, paddingRight: 10 }}>
-                <Image source={require('../assets/images/ProjectMOFI.svg')} style={{ resizeMode: "contain", width: "100%", height: "100%" }} />
+        <View style={styles.style1}>
+            <View style={styles.style2}>
+                <Image source={require('../assets/images/ProjectMOFI.svg')} style={styles.style3} />
             </View>
-            <View style={{ backgroundColor: "#011e36", height: "100%", width: "40%", display: "flex", flexDirection: 'column', justifyContent: 'center' }}>
-                <View style={{ width: "75%", height: "10%" }}>
-                    <Text style={{ fontSize: 25, color: "white", paddingBottom: 10 }}>
+            <View style={styles.style4}>
+                <View style={styles.style14}>
+                    <Text style={styles.style11}>
                         Log In
                     </Text>
-                    <View style={{ backgroundColor: "#02e6c8", height: 2, width: "100%", marginLeft: -10 }}></View>
+                    <View style={styles.style9}></View>
                 </View>
-                <View style={{ width: "100%" }}>
-                    <Text style={{ fontSize: 20, color: "white", paddingBottom: 5 }}>Username*</Text>
-                    <TextInput style={{ borderRadius: 7, marginBottom: 20, backgroundColor: "white", width: "70%" }} />
-                    <Text style={{ fontSize: 20, color: "white", paddingBottom: 5 }}>Password*</Text>
-                    <TextInput style={{ borderRadius: 7, marginBottom: 20, backgroundColor: "white", width: "70%" }} />
+                <View style={styles.style6}>
+                    <Text style={styles.style8}>Username*</Text>
+                    <TextInput style={styles.style7} />
+                    <Text style={styles.style8}>Password*</Text>
+                    <TextInput style={styles.style7} />
 
-                    <TouchableOpacity style={{ backgroundColor: "#02e6c8", height: 30, width: "50%", borderRadius: 7, alignItems: 'center' }}>
-                        <Text style={{ fontSize: 20, color: "white" }}>Login</Text>
+                    <TouchableOpacity style={styles.style12}>
+                        <Text style={styles.style13}>Login</Text>
                     </TouchableOpacity>
                 </View>
             </View>
         </View>
     );
-
 }
+
+    const styles = StyleSheet.create({
+        style1: {
+            flexDirection: "row",
+            backgroundColor: "#011e36",
+            height: "100%",
+            padding: 10
+        },
+
+        style2: {
+            backgroundColor: "#011e36",
+            height: "100%",
+            width: "60%",
+            paddingTop: 10,
+            paddingRight: 10
+        },
+
+        style3: {
+            resizeMode: "contain",
+            width: "100%",
+            height: "100%"
+        },
+
+        style4: {
+            backgroundColor: "#011e36",
+            height: "100%",
+            width: "40%",
+            display: "flex",
+            flexDirection: 'column',
+            justifyContent: 'center'
+        },
+
+        style5: {
+            backgroundColor: "#02e6c8",
+            height: 2,
+            width: "100%",
+            marginLeft: -10
+        },
+
+        style6: {
+            width: "100%"
+        },
+
+        style7: {
+            borderRadius: 7,
+            marginBottom: 20,
+            backgroundColor: "white",
+            width: "70%"
+        },
+
+        style8: {
+            fontSize: 20,
+            color: "white",
+            paddingBottom: 5
+        },
+
+        style9: {
+            backgroundColor: "#02e6c8",
+            height: 2,
+            width: "100%",
+            marginLeft: -10
+        },
+
+        style11: {
+            fontSize: 25,
+            color: "white",
+            paddingBottom: 10
+        },
+
+        style12: {
+            backgroundColor: "#02e6c8",
+            height: 30,
+            width: "50%",
+            borderRadius: 7,
+            alignItems: 'center'
+        },
+
+        style13: {
+            fontSize: 20,
+            color: "white"
+        },
+
+    });
 
 export default SeventhPage;
